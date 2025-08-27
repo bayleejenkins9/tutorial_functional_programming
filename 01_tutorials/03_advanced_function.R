d2cfbdc791a8a5b35c3cb618cb8b4b83252c2c07
Baylee Jenkins <bjenkins@accesslex.org>
2025-08-27 11:11:43 -0400

    advanced function finished

--- 01_tutorials/03_advanced_function.R
+++ 01_tutorials/03_advanced_function.R
@@ -12,7 +12,7 @@
 library(tidyverse)
 
 ## Functions
-source("...")
+source("R/Utils.R")
 
 # 2. Load data ------------------------------------------------------------
 
@@ -22,15 +22,45 @@ iris_tbl <- as_tibble(iris)
 # 3. Analyze data ---------------------------------------------------------
 
 ## Calculate mean of numeric variables in Iris dataset, and sort them
-
+iris_tbl |> 
+    calc_iris_mean()
 
 
 # 4. General function -----------------------------------------------------
 
 ## https://ggplot2.tidyverse.org/reference/tidyeval.html
 
-## Apply to other datasets
 
+## Function
+calc_numeric_mean <- function(data, group) {
+    data |> 
+        summarize(
+            across(
+                where(is.numeric), \(x) mean(x, na.rm = TRUE)
+                # \ Creates an anonymous function
+            ), 
+            .by = {{ group }} #{} This tells function to look for the group in the
+            # function arguments and not in the dataset
+        ) |> 
+        pivot_longer(
+            cols      = where(is.numeric),
+            names_to  = "measure",
+            values_to = "mean"
+        ) |> 
+        group_by(measure) |> 
+        arrange(
+            desc(mean),
+            .by_group = TRUE
+        ) |> 
+        ungroup()
+}
+
+## Apply to other datasets
+airquality |>
+  calc_numeric_mean(
+      group = Month
+  ) 
+    
 
 
 
--- R/Utils.R
+++ R/Utils.R
@@ -14,3 +14,51 @@ convert_ft_to_m <- function(x) {
 calculate_volume <- function(diameter, height) {
     pi / 4 * (diameter / 100)^2 * height
 }
+
+
+# FUNCTION: Summarize iris dataset
+calc_iris_mean <- function(data) {
+    data |> 
+        summarize(
+            across(
+                where(is.numeric), mean
+            ), 
+            .by = Species # This is a shortcut for using group_by() and then ungroup()
+        ) |> 
+        pivot_longer(
+            cols      = where(is.numeric),
+            names_to  = "measure",
+            values_to = "mean"
+        ) |> 
+        group_by(measure) |> 
+        arrange(
+            desc(mean),
+            .by_group = TRUE
+        ) |> 
+        ungroup()
+}
+
+
+## FUNCTION: summarize mean of numeric variables
+calc_numeric_mean <- function(data, group) {
+    data |> 
+        summarize(
+            across(
+                where(is.numeric), \(x) mean(x, na.rm = TRUE)
+                # \ Creates an anonymous function
+            ), 
+            .by = {{ group }} #{} This tells function to look for the group in the
+            # function arguments and not in the dataset
+        ) |> 
+        pivot_longer(
+            cols      = where(is.numeric),
+            names_to  = "measure",
+            values_to = "mean"
+        ) |> 
+        group_by(measure) |> 
+        arrange(
+            desc(mean),
+            .by_group = TRUE
+        ) |> 
+        ungroup()
+}
