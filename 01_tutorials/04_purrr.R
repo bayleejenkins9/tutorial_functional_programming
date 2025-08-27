e5d36b30df38e646e07c4847157605019f849998
Baylee Jenkins <bjenkins@accesslex.org>
2025-08-27 16:58:54 -0400

    purr pkg part one

--- 01_tutorials/04_purrr.R
+++ 01_tutorials/04_purrr.R
@@ -26,9 +26,19 @@ objects_list <- list("peach", "pear", "cherry", "strawberry", "blackberry")
 ## 3.1. Using for loop ---------------------------
 
 ## Create an empty list
+messages_list <- list()
 
+for (i in 1:length(objects_list)){
+    
+    messages_list[[i]] <- str_glue("This is a {objects_list[[i]]}")
+}
 
 ## 3.2. Using functional programming -------------
+
+messages_list <- map(
+    objects_list,
+    \(fruit) str_glue("This fruit is a {fruit}")
+)
 
 
 # 4. Model by species -----------------------------------------------------
@@ -44,6 +54,17 @@ species_vec <- unique(iris_tbl$Species)
 filtered_lst <- list()
 
 ## Iterate to filter each species
+for (species in 1:length(species_vec)){
+    ## Filter observations
+    iris_filtered <- iris_tbl |> 
+        filter(
+            Species == species_vec[species]
+        )
+    ## Calculate linear model
+    iris_filtered_lm <- lm(Petal.Length ~ Sepal.Length, data = iris_filtered)
+    ## Get summary
+    filtered_lst[[species]] <- summary(iris_filtered_lm)
+}
 
 
 ## 4.2. Using functional programming --------------
